ef77b0ac9804478a06fcd776332d954ec2038418
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-07-22 22:48:48 +0200

    Character orign added 💻

--- public/app.js
+++ public/app.js
@@ -20,21 +20,37 @@ const getEpisodes = async () => {
 const showCharacters = async () => {
   await getData().then((data) => {
     const characters = data.results;
+    console.log(characters[0]);
     characters.forEach((character) => {
       charactersContainer.innerHTML += `
-        <div class="character-container d-flex p-4 border-bottom border-3" id="${character.id}">
-        <img class="character-image justify-content-center align-items-center" src="${character.image}" width="100" height="120">
+        <div class="character-container d-flex p-4 border-bottom border-3" id="${
+          character.id
+        }">
+        <img class="character-image justify-content-center align-items-center" src="${
+          character.image
+        }" width="100" height="120">
         <div class="character-info p-4">
         <h1> ${character.name} - #${character.id} </h1>
-        <h2>${character.species}</h2>
-        <h3><span class="fw-bold">Last Known Location:</span> ${character.location.name}</h3>
+        <h2>${character.species} - ${character.gender}</h2>
+        <h3><span class="fw-bold">Last Known Location:</span> ${
+          character.location.name
+        }</h3> 
+        <h3><span class="fw-bold">Origin:</span> ${character.origin.name}</h3> 
+        <h3><span class="fw-bold">Inserted  At:</span> ${new Date(
+          character.created
+        ).toLocaleDateString()}</h3>
         <h3><span class="fw-bold">Status:</span><span class="status-circle p-4 rounded-circle alive"></span></h3>
-        <a href="${character.episode[0]}" target="_blank" class="btn btn-dark btn-sm">First Apparition</a>
+        <a href="${
+          character.episode[0]
+        }" target="_blank" class="btn btn-dark btn-sm">First Apparition</a>
+        <a href="${
+          character.url
+        }" target="_blank" class="btn btn-dark btn-sm">JSON Format</a>
         </div>
         </div>
       `;
       const statusCircle = document.querySelector(".status-circle");
-      if (character.status == "Alive") {
+      if (character.status === "alive") {
         statusCircle.classList.add("alive");
       } else {
         statusCircle.classList.add("dead");
