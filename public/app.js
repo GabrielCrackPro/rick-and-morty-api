c6472bd3e216c2c7c8d9b0d2da044de84dce1c0a
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-07-22 16:53:25 +0200

    Live status added 💻

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 let actualPage = 1;
+let pagesLimit = 34;
 
 let CHARACTERS_URL = `https://rickandmortyapi.com/api/character/?page=${actualPage}`;
 let EPISODES_URL = "https://rickandmortyapi.com/api/episode";
@@ -7,6 +8,7 @@ const charactersContainer = document.querySelector(".character-container");
 const nextPageButton = document.querySelector(".next-btn");
 const prevoiusPageButton = document.querySelector(".previous-btn");
 
+const pageCounter = document.querySelector(".page-counter");
 const getData = async () => {
   let data = await fetch(CHARACTERS_URL).then((response) => response.json());
   return data;
@@ -23,25 +25,37 @@ const showCharacters = async () => {
         <div class="character-container d-flex p-4 border-bottom border-3" id="${character.id}">
         <img class="character-image justify-content-center align-items-center" src="${character.image}" width="100" height="120">
         <div class="character-info p-4">
-        <h1><i class="fas fa-user"></i> ${character.name}</h1>
+        <h1> ${character.name} - #${character.id} </h1>
         <h2>${character.species}</h2>
         <h3><span class="fw-bold">Last Known Location:</span> ${character.location.name}</h3>
+        <h3><span class="fw-bold">Status:</span><span class="status-circle p-4 rounded-circle alive"></span></h3>
+        <a href="${character.episode[0]}" target="_blank" class="btn btn-dark btn-sm">First Apparition</a>
         </div>
         </div>
       `;
+      const statusCircle = document.querySelector(".status-circle");
+      if (character.status == "Alive") {
+        statusCircle.classList.add("alive");
+      } else {
+        statusCircle.classList.add("dead");
+      }
     });
   });
 };
-
+pageCounter.innerHTML = `Page ${actualPage} of ${pagesLimit}`;
 nextPageButton.addEventListener("click", () => {
   actualPage += 1;
   CHARACTERS_URL = `https://rickandmortyapi.com/api/character/?page=${actualPage}`;
   charactersContainer.innerHTML = "";
   showCharacters();
+  pageCounter.innerHTML = `Page ${actualPage} of ${pagesLimit}`;
 });
 
 if (actualPage === 1) {
-  prevoiusPageButton.classList.add("disabled");
+  prevoiusPageButton.style.cursor = "not-allowed";
+}
+if (actualPage == pagesLimit) {
+  nextPageButton.classList.add("disabled");
 }
 prevoiusPageButton.addEventListener("click", () => {
   if (actualPage > 1) {
@@ -49,6 +63,8 @@ prevoiusPageButton.addEventListener("click", () => {
     CHARACTERS_URL = `https://rickandmortyapi.com/api/character/?page=${actualPage}`;
     charactersContainer.innerHTML = "";
     showCharacters();
+    pageCounter.innerHTML = `Page ${actualPage} of ${pagesLimit}`;
   }
 });
+
 window.onload = showCharacters;
